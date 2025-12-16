60aa9eb149042265df3ebf7bcb8b775da8af5f6a
drazil <guido.bonerz@drazil.de>
2025-12-16 16:25:17 +0100

    add list filter

--- commands/file.go
+++ commands/file.go
@@ -34,7 +34,7 @@ var mountedDiskImage []byte
 
 func RemoteLsCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "ls [path/diskimage] [filter]",
+		Use:     "ls [path/diskimage]",
 		Short:   "List files of the internal drive like USB Stick, SD Card, DiskImages, etc.",
 		Long:    "List files of the internal drive like USB Stick, SD Card, DiskImages, etc.",
 		GroupID: "file",
@@ -51,43 +51,48 @@ func RemoteLsCommand() *cobra.Command {
 			}
 
 			for _, entry := range entries {
-				if entry.Type == ftp.EntryTypeFolder {
-					fmt.Printf("%s %s%s\n", extensionIcon["dir"], util.Green, entry.Name)
-				} else {
-					var address = ""
-					suffix := getSuffix(entry)
-					showStart, _ := cmd.Flags().GetBool("showaddress")
-					if showStart {
-						address = "|----"
-						var err error
-						var r *ftp.Response
-						if suffix == "prg" {
+				f, _ := cmd.Flags().GetString("filter")
+				if filter(entry.Name, f) {
+					if entry.Type == ftp.EntryTypeFolder {
+						fmt.Printf("%s %s%s\n", extensionIcon["dir"], util.Green, entry.Name)
+					} else {
+						var address = ""
+						suffix := getSuffix(entry)
+						showStart, _ := cmd.Flags().GetBool("memaddress")
+						if showStart {
+							address = "|----"
+							var err error
+							var r *ftp.Response
+							if suffix == "prg" {
 
-							r, err = c.Retr(fmt.Sprintf("%s/%s", path, entry.Name))
-							if err != nil {
-								log.Printf("Error: %v\n", err)
-								continue
+								r, err = c.Retr(fmt.Sprintf("%s/%s", path, entry.Name))
+								if err != nil {
+									log.Printf("Error: %v\n", err)
+									continue
+								}
+								var content [2]byte
+								io.ReadFull(r, content[:])
+								r.Close()
+								address = fmt.Sprintf("|%04x", util.GetWordFromArray(0, content[:]))
+
 							}
-							var content [2]byte
-							io.ReadFull(r, content[:])
-							r.Close()
-							address = fmt.Sprintf("|%04x", util.GetWordFromArray(0, content[:]))
 
 						}
+						icon := extensionIcon[suffix]
+						if icon == "" {
+							icon = extensionIcon["default"]
+						}
+						valueParts := strings.Fields(humanize.Bytes(entry.Size))
+						fmt.Printf("%s %s%-6s%s|%s%s\n", icon, util.Gray, fmt.Sprintf("%6s %-3s", valueParts[0], valueParts[1]), address, util.Blue, entry.Name)
 					}
-					icon := extensionIcon[suffix]
-					if icon == "" {
-						icon = extensionIcon["default"]
-					}
-					valueParts := strings.Fields(humanize.Bytes(entry.Size))
-					fmt.Printf("%s %s%-6s%s|%s%s\n", icon, util.Gray, fmt.Sprintf("%6s %-3s", valueParts[0], valueParts[1]), address, util.Blue, entry.Name)
 				}
 			}
-			//cmd.Flags().Set("showaddress", "false")
 
 		},
 	}
-	cmd.Flags().BoolP("showaddress", "s", false, "displays the start address of a program if possible")
+	cmd.Flags().BoolP("memaddress", "m", false, "displays the start address of a program if possible")
+	cmd.Flags().StringP("filter", "f", "", "filters the list by a match pattern like '*.prg'")
+	//cmd.Flags().BoolP("sort", "s", false, "displays the start address of a program if possible")
 
 	return cmd
 }
@@ -101,6 +106,7 @@ func RemoteCdCommand() *cobra.Command {
 		Args:    cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			var c = network.GetFtpConnection()
+
 			path := ""
 			if len(args) > 0 {
 				path = args[0]
@@ -139,4 +145,28 @@ func RemoteCdCommand() *cobra.Command {
 
 func getSuffix(entry *ftp.Entry) string {
 	return re.FindStringSubmatch(strings.ToLower(entry.Name))[1]
+}
+func filter(name string, pattern string) bool {
+	match := false
+
+	f := 0
+	if strings.HasPrefix(pattern, "*") {
+		f |= 1
+		pattern = pattern[1:]
+	}
+	if strings.HasSuffix(pattern, "*") {
+		f |= 2
+		pattern = pattern[:len(pattern)-1]
+	}
+	switch f {
+	case 1:
+		match = strings.HasSuffix(name, pattern)
+	case 2:
+		match = strings.HasPrefix(name, pattern)
+	case 3:
+		match = strings.Contains(name, pattern)
+	default:
+		match = true
+	}
+	return match
 }
